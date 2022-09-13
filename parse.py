8d3789d6c37a38bf28a3fade9abcc6b63bc28071
QoltyQ <janathan186@gmail.com>
2022-09-14 04:09:17 +0600

    url

--- parse.py
+++ parse.py
@@ -198,4 +198,4 @@ class Parser:
 if __name__ == '__main__':
     print('Parsing Started!')
     p = Parser()
-    p.parse_car('135801340', None, '14 сентября', '/a/show/135801340')
+    p.parse_car('134765354', None, '14 сентября', '/a/show/134765354')
