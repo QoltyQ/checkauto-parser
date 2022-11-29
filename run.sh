8b789b53cd7f4da6b75542445b985e40a6d6db63
QoltyQ <janathan186@gmail.com>
2022-11-29 16:13:30 +0600

    bug fix

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ pow=2
 
 nohup python3 get_new_cars.py > log/get_new_cars.log 2>&1> get_new_cars.out &
 nohup python3 update_cars.py > log/update_cars.log 2>&1> update_cars.out &
+nohup python3 bot.py > log/bot.log 2>&1> bot.out &
+
 
 for i in {1..6}
 do
